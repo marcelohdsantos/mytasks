aa090290066d28558a5aa3d328c2f74f5aff202f
marcelohdsantos <marcelodossantos732@yahoo.com.br>
2022-08-09 10:29:59 -0400

    Implementacao teste de listar tarefas.

--- tasks/tarefas/tests/test_tasks_get.py
+++ tasks/tarefas/tests/test_tasks_get.py
@@ -2,6 +2,8 @@ import pytest
 from django.urls import reverse
 from pytest_django.asserts import assertContains
 
+from tasks.tarefas.models import Tarefa
+
 
 @pytest.fixture
 def resposta(client):
@@ -19,3 +21,21 @@ def test_formulario_present(resposta):
 
 def test_botao_salvar_presente(resposta):
     assertContains(resposta, '<button type="submit"')
+
+
+@pytest.fixture
+def lista_de_tarefas_pendentes(db):
+    return [
+        Tarefa(nome='Tarefa 1'),
+        Tarefa(nome='Tarefa 2'),
+    ]
+
+
+@pytest.fixture
+def resposta_com_lista_de_tarefas(client, test_lista_de_tarefas_pendentes):
+    resp = client.get(reverse('tarefas:home'))
+    return resp
+
+def test_lista_de_tarefas_pendentes_present(resposta_com_lista_de_tarefas, lista_de_tarefas_pendentes):
+    for tarefa in lista_de_tarefas_pendentes:
+        assertContains(resposta, tarefa.home)
--- tasks/tarefas/views.py
+++ tasks/tarefas/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 from django.urls import reverse
 
 from tasks.tarefas.forms import TarefaNovaForm
+from tasks.tarefas.models import Tarefa
 
 
 def home(request):
@@ -14,5 +15,7 @@ def home(request):
             form.save()
             return HttpResponseRedirect(reverse('tarefas:home'))
         else:
-            return render(request, 'tarefas/home.html', {'form': form}, status=400)
-    return render(request, 'tarefas/home.html')
+            tarefas_pendentes = Tarefa.objects.filter(feita=False).all()
+            return render(request, 'tarefas/home.html', {'form': form, 'tarefas_pendentes': tarefas_pendentes}, status=400)
+    tarefas_pendentes = Tarefa.objects.filter(feita=False).all()
+    return render(request, 'tarefas/home.html', {'tarefas_pendentes': tarefas_pendentes})
